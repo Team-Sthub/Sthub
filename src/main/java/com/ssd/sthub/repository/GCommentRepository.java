a6e82a15bbb3cf59ce565fce5e03d11df39ad27a
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-04-28 21:07:50 +0900

    feat(#20) 공동구매 댓글, 신청 Service & Participation accept 변경

--- src/main/java/com/ssd/sthub/repository/GCommentRepository.java
+++ src/main/java/com/ssd/sthub/repository/GCommentRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GCommentRepository extends JpaRepository<GComment, Long> {
 
+    GComment save(GComment gComment);
     List<GComment> findAllByGroupBuying(GroupBuying groupBuying);
 }
