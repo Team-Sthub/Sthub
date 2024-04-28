d0308cd47035f080008e8fe0763415934894b2bd
yeon015 <lizkitty@naver.com>
2024-04-28 14:16:28 +0900

    feat(#1) : 쪽지&거래후기&신고 entity작성

---
+++ src/main/java/com/ssd/sthub/domain/Complaint.java
@@ -0,0 +1,57 @@
+package com.ssd.sthub.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Complaint {
+
+    // 신고 식별값
+    @Id
+    @Column(name="complaintId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long complaintId;
+
+    // 조건 (각 항목은 노션에 정리되어 있음)
+    @Column(nullable = false)
+    private Boolean tag1;
+
+    @Column(nullable = false)
+    private Boolean tag2;
+
+    @Column(nullable = false)
+    private Boolean tag3;
+
+    @Column(nullable = false)
+    private Boolean tag4;
+
+    @Column(nullable = false)
+    private Boolean tag5;
+
+    @Column(nullable = false)
+    private Boolean tag6;
+
+    @Column(nullable = false)
+    private Boolean tag7;
+
+    @Column(nullable = false)
+    private Boolean tag8;
+
+    @Column(nullable = false)
+    private Boolean tag9;
+
+    @CreatedDate
+    @Column(name = "createdAt", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    // 공동구매 조인
+
+    // 중고거래 조인
+}
