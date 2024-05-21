9a1fabc06f152b000fdb7095c4f26514b88ffe6f
yeon015 <lizkitty@naver.com>
2024-05-21 23:36:59 +0900

    feat(#59) : 공동구매 상세 조회 ui 일부 구현

--- src/main/java/com/ssd/sthub/controller/GroupBuyingController.java
+++ src/main/java/com/ssd/sthub/controller/GroupBuyingController.java
@@ -9,47 +9,54 @@ import com.ssd.sthub.response.SuccessResponse;
 import com.ssd.sthub.service.GroupBuyingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping
+@Controller
+@RequestMapping("/groupBuying")
 public class GroupBuyingController {
 
     private final GroupBuyingService groupBuyingService;
 
     // 공동구매 전체 조회
-    @GetMapping("/groupBuying/list/{category}")
+    @GetMapping("/list/{category}")
     public ResponseEntity<SuccessResponse<GroupBuyingListDTO>> getAllGroupBuying(@PathVariable Category category, @RequestParam int pageNum) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.getAllGroupBuying(category, pageNum)));
     }
 
+    // 공동구매 게시글 상세 조회 (일단 뷰 확인용)
+    @GetMapping("/detail")
+    public String showDetail() {
+        return "thyme/groupBuying/detail";
+    }
+
     // 공동구매 게시글(상세) 조회 (작성자 확인은 controller에서 하고 뷰 설정)
-    @GetMapping("/groupBuying/detail")
+    @GetMapping("/detail/test")
     public ResponseEntity<SuccessResponse<GroupBuying>> getGroupBuying(@RequestHeader Long memberId, Long groupBuyingId) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.getGroupBuying(memberId, groupBuyingId)));
     }
 
     // 공동구매 게시글 작성
-    @PostMapping("/groupBuying/create")
+    @PostMapping("/create")
     public ResponseEntity<SuccessResponse<GroupBuying>> postGroupBuying(@RequestHeader Long memberId, @RequestBody GroupBuyingDetailDTO groupBuyingDetailDTO) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.postGroupBuying(memberId, groupBuyingDetailDTO)));
     }
 
     // 공동구매 게시글 수정
-    @PatchMapping("/groupBuying/update")
+    @PatchMapping("/update")
     public ResponseEntity<SuccessResponse<GroupBuying>> updateGroupBuying(@RequestHeader Long memberId, @RequestBody GroupBuyingDetailDTO groupBuyingDetailDTO) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.updateGroupBuying(memberId, groupBuyingDetailDTO)));
     }
 
     // 공동구매 게시글 삭제
-    @DeleteMapping("/groupBuying/delete")
+    @DeleteMapping("/delete")
     public ResponseEntity<SuccessResponse<String>> updateGroupBuying(@RequestHeader Long memberId, Long groupBuyingId) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.deleteGroupBuying(memberId, groupBuyingId)));
     }
 
     // 마이페이지 - 공구 모집 조회
-    @GetMapping("/groupbuying/mylist")
+    @GetMapping("/mylist")
     public ResponseEntity<SuccessResponse<GroupBuyingListDTO>> getAllGroupBuyingByMemberId(@RequestHeader Long memberId, @RequestParam int pageNuM) {
         return ResponseEntity.ok(SuccessResponse.create(groupBuyingService.getAllGroupBuyingByMemberId(memberId, pageNuM)));
     }
