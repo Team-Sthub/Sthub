eacdc0d23e255c43ab1341152aedf64be814b2f7
yeon015 <lizkitty@naver.com>
2024-05-31 01:33:38 +0900

    fix(#59) : 공동구매 게시글 수정 진행중 & 파일 내용 합치기

--- src/main/resources/static/js/groupBuying/detail.js
+++ src/main/resources/static/js/groupBuying/detail.js
@@ -13,3 +13,72 @@ document.addEventListener('DOMContentLoaded', function () {
         categoryElement.textContent = categoryMap[categoryValue];
     }
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('update');
+    const groupBuyingId = document.getElementById('groupBuyingId').value;
+    const statusElement = document.getElementById('status');
+
+    // 기존 상태 값을 저장
+    let originalStatus = statusElement.innerText.trim();
+
+    if (statusElement) { // 요소가 존재하는지 확인
+        statusElement.addEventListener('click', () => {
+            // 상태 변경
+            if (statusElement.innerText.trim() === '모집중') {
+                statusElement.innerText = '모집완료';
+            } else if (statusElement.innerText.trim() === '모집완료') {
+                statusElement.innerText = '모집중';
+            }
+        });
+    } else {
+        console.log('Status element not found');
+    }
+
+    const updateBtn = document.getElementById('updateBtn');
+    updateBtn.addEventListener('click', (event) => {
+        event.preventDefault(); // 기본 이벤트 제거 (페이지 새로고침 방지)
+
+        const formData = new FormData();
+        var requestData = {
+            groupBuyingId : document.getElementById('groupBuyingId').value,
+            title: document.getElementById('title').value,
+            category : document.getElementById('category').value,
+            product : document.getElementById('product').value,
+            price: document.getElementById('price').value,
+            deadline : document.getElementById('deadline').value,
+            chatLink : document.getElementById('chatLink').value,
+            meetingPlace : document.getElementById('meetingPlace').value,
+            content : document.getElementById('content').value,
+            status : statusElement.innerText.trim()
+        }
+
+        formData.append('request', new Blob([JSON.stringify(requestData)], { type: 'application/json' }));
+
+        // 파일 업로드 input 요소
+        const fileInput = document.getElementById('file-upload');
+        // 파일이 있는지 확인
+
+        for (let i = 0; i < fileInput.files.length; i++) {
+            formData.append('imgUrl', fileInput.files[i]);
+        }
+
+
+        // fetch 요청
+        fetch(`/groupBuying/update?groupBuyingId=${groupBuyingId}`, {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = `/groupBuying/detail?groupBuyingId=${groupBuyingId}`;
+
+                }else{
+                    throw new Error('수정 실패');
+                }
+            }).catch(error => {
+                console.error('Error:', error);
+                alert('수정 실패');
+            });
+    });
+});
